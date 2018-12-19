862436dab99dc93252aa6735c0a32942bce1d16f
Łukasz Krause <lukasz.krause@onet.eu>
2018-12-19 09:20:26 +0100

    Adding prompt from user

--- Diki.java
+++ Diki.java
@@ -1,4 +1,3 @@
-
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 
@@ -6,42 +5,54 @@ import java.util.Scanner;
 
 public class Diki {
 
-    public static void main(String[] args) {
+    static Scanner scan = new Scanner(System.in);
+    static String userInput = null;
 
+    public static void main(String args[]) {
+        CheckWord();
+        while (true) {
+            System.out.print("Czy chcesz wyszukać następne hasło (T/N) ?");
+            userInput = scan.next();
+            if (userInput.equalsIgnoreCase("T"))
+                CheckWord();
+            else if (userInput.equalsIgnoreCase("N")) {
+                System.out.print("Dziękuję i miłego dnia życzę !! ");
+                System.out.println("Łukasz Krause - www.and-it.pl");
+                break;
+            } else {
+                System.out.print("Wprowadż T albo N (Y/N) only !");
+            }
+
+        }
+
+    }
+
+    public static void CheckWord() {
         Scanner scan = new Scanner(System.in);
         System.out.print("Wpisz hasło do wyszukania: ");
         String word = scan.next();
 
-        System.setProperty("webdriver.chrome.driver","C:\\Selenium\\chromedriver.exe");
+        System.setProperty("webdriver.chrome.driver", "C:\\Selenium\\chromedriver.exe");
 
         ChromeOptions options = new ChromeOptions();
         options.setHeadless(true);
-
 
         ChromeDriver driver = new ChromeDriver(options);
         driver.get("http://www.diki.pl");
 
         System.out.println(driver.getTitle());
 
-
-
         driver.findElementByCssSelector("input[type='search']").sendKeys(word);
         driver.findElementByCssSelector("button[type='submit']").click();
 
-        try
-        {
+        try {
             System.out.println(driver.findElementByClassName("dictionaryEntity").getText());
-        }
-        catch(Exception e)
-        {
+        } catch (Exception e) {
             System.out.println("Brak hasła w słowniku");
-            System.out.println(driver.findElementByCssSelector("#contentWrapper > div.dikiBackgroundBannerPlaceholder > div.dictionarySuggestions").getText());
+            System.out.println(driver.findElementByCssSelector("div[class='dictionarySuggestions']").getText());
         }
 
 
         driver.close();
-
-
-
     }
 }
